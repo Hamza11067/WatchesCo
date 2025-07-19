046abb2b3144482b9b77cce95d2f3ea912604ac5
Hamza11067 <hamzakhalid1067@gmail.com>
2025-07-19 11:35:25 +0500

    Contact and Services page in progress

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -27,7 +27,6 @@ export default function ProductList() {
           <div className="flex justify-between items-center text-white">
             <div>
               <h2 className="text-lg font-semibold mt-2">{product.title}</h2>
-              {/* <p className="text-gray-600">{product.description}</p> */}
               <p className="text-green-600 font-semibold">${product.price}</p>
             </div>
             <FaCartShopping />
